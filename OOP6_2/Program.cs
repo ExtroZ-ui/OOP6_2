d1c2f5b48cee4b5233a22baf6a2483da2803c02d
Extro <22ff2296@gmail.com>
2020-06-15 12:06:34 +0500

    Добавьте файлы проекта.

---
+++ OOP6_2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP6_2
+{
+        class String
+    {
+        private string Line;
+
+        public String()
+        {
+            Line = "Пусто";
+        }
+
+        public String(string Line)
+        {
+            if (Line.Length > 10)
+                this.Line = "Слишком длинная строка";
+            else
+                this.Line = Line;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(Line);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String st = new String();
+            st.Display();
+            String st1 = new String("12345678910");
+            st1.Display();
+            String st2 = new String("123");
+            st2.Display();
+
+            Console.ReadKey();
+        }
+    }
+    
+}
